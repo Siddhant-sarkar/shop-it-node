30c1ff9d4966cdc3c993e40c64fe534a6409f897
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2022-04-17 22:03:12 +0530

    THis is so good

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -20,5 +21,5 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-
+console.log("<--------------------->")
 app.listen(3000);
