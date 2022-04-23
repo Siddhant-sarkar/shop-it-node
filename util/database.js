adaf65c3e0ebc7b593de2210f43a29a67aebde49
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2022-04-23 09:39:12 +0530

    return to the style

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    "mongodb+srv://Sid:Sid.8349826991@database-fire.th4df.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   )
-    .then(client => {
-      console.log('Connected!');
-      callback(client);
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
